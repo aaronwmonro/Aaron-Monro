230d5d74220889642230a8d927db95a0978fcbc1
aaronwmonro <63673145+aaronwmonro@users.noreply.github.com>
2020-07-13 22:55:07 -0400

    Instagram and maps

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -8,5 +8,28 @@ tns({
     controls: false,
     controlsPosition: "bottom",
     navPosition: "bottom"
-    
+});
+
+tns({
+    container: '.my-slider-2',
+    items: 1,
+    gutter: 15,
+    edgePadding: 50,
+    controls: false,
+    controlsPosition: "bottom",
+    navPosition: "bottom",
+     responsive: {
+          "350": {
+            "items": 1
+          },
+          "768": {
+            "items": 3,
+            gutter:50
+          },
+          "1040": {
+            "items": 4,
+            gutter:100
+          }
+    }
+   
 });
